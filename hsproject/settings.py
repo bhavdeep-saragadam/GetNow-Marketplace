7633f65b3a9ff1483c609e4f6070227d575f5fa6
Bhavdeeep <bhavdeep.saragadam@gmail.com>
2024-02-04 18:53:09 -0500

    postgres changes

--- hsproject/settings.py
+++ hsproject/settings.py
@@ -79,11 +79,15 @@ WSGI_APPLICATION = 'hsproject.wsgi.application'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'URL': 'postgresql://postgres:Be1G356d354bCF-adbEG-fC3c52gFab4@postgres.railway.internal:5432/railway',
+        'NAME': 'railway',
+        'USER': 'postgres',
+        'PASSWORD': 'Be1G356d354bCF-adbEG-fC3c52gFab4',
+        'HOST': 'monorail.proxy.rlwy.net',
+        'PORT': 11601,
     }
 }
-
 
 # Password validation
 # https://docs.djangoproject.com/en/4.0/ref/settings/#auth-password-validators
